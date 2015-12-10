16f3ed9764a0ed8e1721dd1703b5e2beb8fa6267
Alexander A Korotkikh <code@aak.io>
2015-12-10 20:26:21 +0300

    create a singleton for overwatching several drones at once and performing operations on them

---
+++ examples/tower-bf4.js
@@ -0,0 +1,21 @@
+// Bootstrap all scripts here
+
+import getFeeds from '../tower-scripts/tower-feeds.js';
+
+import hardKick from '../tower-scripts/hard-kick.js';
+import setLive from '../tower-scripts/set-live.js';
+import basicCommands from '../tower-scripts/basic-commands.js';
+import logger from '../tower-scripts/logger.js';
+
+(async () => {
+  const { feeds, tower } = await getFeeds();
+
+  hardKick(tower.listPlayers, tower.exec,
+    feeds.teamSetup, feeds.joinLeave, feeds.status);
+
+  setLive(tower.exec, tower.setMeta, feeds.commands);
+
+  logger(feeds.status, feeds.kills, feeds.chat, feeds.joinLeave);
+
+  basicCommands(tower.exec, feeds.commands);
+})();
